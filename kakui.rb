aa0a21a6d0384ff89e42d46d94c62fa05c019bac
KentaYamaguchi <kytiken@gmail.com>
2012-11-22 15:13:48 +0900

    first commit

---
+++ kakui.rb
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+require './twitter_config.rb'
+
+class Kakui
+  attr_reader :members
+  def initialize
+    @members = []
+  end
+
+  def join member
+    @members << member
+  end
+
+  def destination
+    repry_string = ""
+    @members.each do |member|
+      repry_string << "@" + member + " "
+    end
+    return repry_string
+  end
+
+  def scan tweet
+    if descern(tweet)
+      case descern(tweet)
+      when "join"
+        sentence = tweet.split(" ")
+        join(sentence[2])
+      end
+    end
+  end
+
+  def descern tweet
+    if tweet =~ /@各位 /
+      sentence = tweet.split(" ")
+      return sentence[1]
+    end
+    return nil
+  end
+
+  def create_repry_text text
+    repry_text = destination
+    repry_text << text
+    return repry_text
+  end
+
+  def repry text
+    repry_text = create_repry_text(text)
+    Twitter.update(repry_text)
+  end
+end
---
+++ kakui_spec.rb
@@ -0,0 +1,62 @@
+# coding: utf-8
+
+require './kakui.rb'
+
+describe Kakui, "#join" do
+  it "各位のメンバーがmemberに保存できる" do
+    kakui = Kakui.new
+    kakui.join("hoge")
+    kakui.members.should == ["hoge"]
+  end
+
+  it "各位のメンバーがmemberに2人保存できる" do
+    kakui = Kakui.new
+    kakui.join("hoge")
+    kakui.join("fuga")
+    kakui.members.should == ["hoge", "fuga"]
+  end
+end
+
+describe Kakui, "#destination" do
+  it "各位にリプライを飛ばす宛先の文字列が返される" do
+    kakui = Kakui.new
+    kakui.join("hoge")
+    kakui.join("fuga")
+    kakui.destination.should == "@hoge @fuga "
+  end
+end
+
+describe Kakui, "#scan" do
+  it "ツイートに＠各位 join命令が入っていたらmemberに追加する" do
+    kakui = Kakui.new
+    kakui.scan("@各位 join hoge")
+    kakui.members.should == ["hoge"]
+  end
+end
+
+describe Kakui, "#descern" do
+  it "Kakuiに対する命令かどうかを判別して@各位 joinが入っていたらjoinという文字列を返す" do
+    kakui = Kakui.new
+    kakui.descern("@各位 join hoge").should == "join"
+  end
+
+  it "Kakuiに対する命令かどうかを判別して@各位が入っていなかったらfalseを返す" do
+    kakui = Kakui.new
+    kakui.descern("hogefugapiyo").should == nil
+  end
+end
+
+describe Kakui, "#create_repry_text" do
+  it "Kakuiのメンバーにリプライの文面を作る" do
+    kakui = Kakui.new
+    kakui.join("hoge")
+    kakui.create_repry_text("ツイートの文面").should == "@hoge ツイートの文面"
+  end
+end
+
+describe Kakui, "#repry" do
+  it "Kakuiのメンバーにリプライを送る" do
+    kakui = Kakui.new
+    kakui.join("kytiken")
+  end
+end
